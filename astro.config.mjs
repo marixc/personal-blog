e0b511fd8c950e3b30d44d89406a5d1eb48bc454
marixc <marixc@users.noreply.github.com>
2025-02-20 10:38:28 +0100

    jj

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import react from '@astrojs/react';
+// import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  // integrations: [react()]
   site: 'https://marixc.github.io',
   base: '/personal-blog',
 });
